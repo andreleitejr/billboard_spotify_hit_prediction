fix: Data Leakage remove in features, model as RandomForestClassifier

src/scripts/data.py
@@ -1,108 +1,35 @@
-import joblib
 import numpy as np
 import pandas as pd
-from sklearn.compose import ColumnTransformer
 from sklearn.impute import SimpleImputer
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import OneHotEncoder, OrdinalEncoder, StandardScaler
-from sklearn.pipeline import Pipeline
-from datetime import datetime
 
-index_col = 'Song'
 target = 'Peak Position'
-
 
 def load_data(file_path: str) -> pd.DataFrame:
     """Loads a dataset from a CSV file using 'Id' as the index."""
-    data = pd.read_csv(file_path, index_col=index_col)
+    data = pd.read_csv(file_path, delimiter=',', encoding='ascii')
     return data
 
 
 def feature_engineering(data: pd.DataFrame) -> pd.DataFrame:
-    data['performance'] = data['Peak Position'].apply(categorize_peak)
-    data['energy_level'] = data['Energy'].apply(categorize_energy)
-    data['danceability_level'] = data['Danceability'].apply(categorize_danceability)
-    data['energy_danceability_mean'] = (data['Energy'] + data['Danceability']) / 2
-
-    current_year = datetime.now().year
-    data['time_since_release'] = current_year - data['Release Year']
-    data['log_streams'] = np.log1p(data['Streams'])
-    data['log_daily_streams'] = np.log1p(data['Daily Streams'])
-    data['log_weeks_on_chart'] = np.log1p(data['Weeks on Chart'])
-    data['stream_growth_rate'] = data['Daily Streams'] / (data['Streams'] + 1e-6)
-    data['abs_lyrics_sentiment'] = data['Lyrics Sentiment'].abs()
 
     return data
 
-
-def categorize_peak(position):
-    if 1 <= position <= 10:
-        return 'Great'
-    elif 11 <= position <= 33:
-        return 'Good'
-    elif 31 <= position <= 66:
-        return 'Medium'
-    else:
-        return 'Bad'
-
-
-def categorize_energy(energy):
-    if 0 <= energy <= 0.25:
-        return 'VeryLow'
-    elif 0.26 <= energy < 0.40:
-        return 'Low'
-    elif 0.41 <= energy < 0.60:
-        return 'Medium'
-    else:
-        return 'High'
-
-def categorize_danceability(danceability):
-    if 0 <= danceability <= 0.25:
-        return 'VeryLow'
-    elif 0.26 <= danceability < 0.45:
-        return 'Low'
-    elif 0.46 <= danceability < 0.75:
-        return 'Medium'
-    else:
-        return 'High'
-
-
-def columns_to_drop() -> list:
-    """Returns a list of columns to remove."""
-    return [target, 'Artist', ]
-
-
 def split_data(data: pd.DataFrame) -> tuple[pd.DataFrame, pd.DataFrame, pd.Series, pd.Series]:
-    X = data.drop(columns=columns_to_drop()).copy()
-    y = data[target].copy()
-    y = y.max() - y + 1
-    X_train, X_valid, y_train, y_valid = train_test_split(X, y, train_size=0.8, test_size=0.2, random_state=42)
-    return X_train, X_valid, y_train, y_valid
+    X = data.drop(['Peak Position', 'Hit'], axis=1)
+    X = pd.get_dummies(X, columns=['Genre'], drop_first=True)
+    y = data['Hit']
+    return train_test_split(X, y, test_size=0.3, random_state=42, stratify=y)
 
 
-def preprocess_data(X_train: pd.DataFrame, X_valid: pd.DataFrame, preprocessor_path: str) -> tuple[
-    pd.DataFrame, pd.DataFrame]:
+def preprocess_data(data: pd.DataFrame) -> pd.DataFrame:
     """Applies numerical imputation, categorical encoding, and scaling."""
-    num_cols = X_train.select_dtypes(include=['int64', 'float64']).columns.tolist()
-    cat_cols = X_train.select_dtypes(include=['object']).columns.tolist()
+    num_cols = data.select_dtypes(include=['int64', 'float64']).columns
+    imputer = SimpleImputer(strategy='median')
+    data[num_cols] = imputer.fit_transform(data[num_cols])
 
-    num_transformer = Pipeline([
-        ('imputer', SimpleImputer(strategy='mean')),
-        ('scaler', StandardScaler())
-    ])
+    data_clean = data.drop(['Song', 'Artist'], axis=1)
 
-    cat_transformer = Pipeline([
-        ('imputer', SimpleImputer(strategy='most_frequent')),
-        ('encoder', OneHotEncoder(handle_unknown='ignore', sparse_output=False))
-    ])
+    data_clean['Hit'] = data_clean['Peak Position'].apply(lambda x: 1 if x <= 10 else 0)
 
-    preprocessor = ColumnTransformer([
-        ('num', num_transformer, num_cols),
-        ('cat', cat_transformer, cat_cols),
-    ])
-
-    X_train_transformed = preprocessor.fit_transform(X_train)
-    X_valid_transformed = preprocessor.transform(X_valid)
-    joblib.dump(preprocessor, preprocessor_path)
-
-    return X_train_transformed, X_valid_transformed
+    return data_clean

src/scripts/model.py
@@ -1,9 +1,6 @@
 import joblib
-import numpy as np
-from sklearn.metrics import mean_absolute_error, r2_score, make_scorer, mean_squared_log_error
-from sklearn.model_selection import KFold, cross_val_score
-from xgboost import XGBRegressor
-from sklearn.model_selection import GridSearchCV
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import mean_absolute_error, r2_score, make_scorer, mean_squared_log_error, accuracy_score
 
 
 def load_model(model_path: str):
@@ -12,77 +9,26 @@ def load_model(model_path: str):
     return model
 
 
-def train_model(X_train, X_valid, y_train, y_valid) -> XGBRegressor:
+def train_model(X_train, y_train) -> RandomForestClassifier:
     """Trains an XGBoost regressor with predefined hyperparameters."""
-    params = {
-        'n_estimators': 500,
-        'learning_rate': 0.01,
-        'max_depth': 4,
-        'subsample': 0.8,
-        'colsample_bytree': 0.8,
-    }
 
-    model = XGBRegressor(**params, n_jobs=4)
-    model.fit(X_train, y_train, eval_set=[(X_valid, y_valid)], verbose=False)
+    model = RandomForestClassifier(n_estimators=100, random_state=42)
+    model.fit(X_train, y_train)
 
     return model
 
 
-def grid_search_train_model(X_train, y_train) -> XGBRegressor:
-    """Trains an XGBoost regressor using GridSearchCV with cross-validation."""
-    param_grid = {
-        'n_estimators': [500, 600, 800, 1000],
-        'learning_rate': [0.01, 0.3, 0.05, 0.1],
-        'max_depth': [4, 6, 8],
-        'subsample': [0.5, 0.6, 0.8],
-        'colsample_bytree': [0.4, 0.6, 0.8],
-    }
-
-    model = XGBRegressor(n_jobs=4, verbosity=0)
-
-    grid_search = GridSearchCV(
-        estimator=model,
-        param_grid=param_grid,
-        scoring='r2',
-        cv=5,
-        n_jobs=-1,
-        verbose=1
-    )
-
-    grid_search.fit(X_train, y_train)
-
-    return grid_search.best_estimator_
-
-
-def evaluate_model(model, X_valid, y_valid) -> None:
+def validate_model(model, X_valid, y_valid) -> None:
+    """Calculates the Mean Absolute Error (MAE) and Accuracy."""
     predictions = model.predict(X_valid)
 
     mae = mean_absolute_error(y_valid, predictions)
-    r2 = r2_score(y_valid, predictions)
-    rmsle = np.sqrt(mean_squared_log_error(y_valid, predictions))
+    accuracy = accuracy_score(y_valid, predictions)
 
     print(
-        f'✅ Model evaluation completed.\n'
-        f'🎯 MAE: {mae:.2f}\n'
-        f'📈 RMSLE: {rmsle:.4f}\n'
-        f'🔍 Accuracy (R² Score): {r2:.4f} ({r2:.0%})\n'
-    )
-
-
-def cross_validate_model(model, X_valid, y_valid) -> None:
-    """Performs cross-validation and calculates the Mean Absolute Error (MAE) and Accuracy (R² Score)."""
-    cv = KFold(n_splits=5, shuffle=True, random_state=42)
-
-    mae_scores = -cross_val_score(model, X_valid, y_valid, cv=cv, scoring='neg_mean_absolute_error')
-    mean_mae, std_mae = np.mean(mae_scores), np.std(mae_scores)
-
-    r2_scores = cross_val_score(model, X_valid, y_valid, cv=cv, scoring=make_scorer(r2_score))
-    mean_r2, std_r2 = np.mean(r2_scores), np.std(r2_scores)
-
-    print(
-        f'✅ Cross-validation completed.\n'
-        f'📊 MAE (Cross-Validation): {mean_mae:.2f} (± {std_mae:.2f})\n'
-        f'🔍 Accuracy (R² Score - Cross Validation): {mean_r2:.4f} ({mean_r2:.0%}) (± {std_r2:.2f})\n'
+        f'✅ Validation completed!\n'
+        f'📊 MAE (Cross-Validation): {mae:.2f} (± {mae:.2f})\n'
+        f'🔍 Accuracy: {accuracy:.4f} ({accuracy:.0%})\n'
     )
 
 